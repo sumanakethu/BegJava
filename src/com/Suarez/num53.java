7e3fb6ea90cb9c0a5674b6b2b64361ce972670b5
sumanakethu <kethus22@greenhill.org>
2019-04-16 15:44:43 -0500

    iygg8-0h[0ni

--- src/com/Suarez/num53.java
+++ src/com/Suarez/num53.java
@@ -1,5 +1,9 @@
 package com.Suarez;
-
+/*
+Sumana Kethu
+4/16/19
+writing a program that generates two random numbers between 0 and 100 and prints the smaller of the two numbers.
+ */
 import java.util.Random;
 
 public class num53 {
